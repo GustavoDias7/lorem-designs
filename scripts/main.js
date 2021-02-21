5071db7913966b15b47d39e3a2b6016d28bc5a57
Gustavo <gustavo7dias@gmail.com>
2021-02-21 16:46:38 -0300

    Add zoom script

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
+const body = document.querySelector('body');
 const hamburger = document.getElementById('hamburger');
 const nav_links = document.querySelector('.nav-links');
 const back = document.getElementById('back-icon');
+const zoom_image = document.querySelector('.zoom-image');
+const zoom_figure_image = document.querySelector('.zoom-figure-image');
+const close_zoom_figure_image = document.querySelector('.close-zoom-figure-image');
 
 hamburger.addEventListener('click', () => {
     nav_links.style.right = '0px';
@@ -8,4 +12,15 @@ hamburger.addEventListener('click', () => {
 
 back.addEventListener('click', () => {
     nav_links.style.right = '-200px';
+});
+
+document.addEventListener('click', event => {
+    if (!event.target.classList.contains('open-image')) return;
+    zoom_figure_image.classList.toggle('disable');
+    let little_image = event.target.parentNode.previousElementSibling.currentSrc;
+    zoom_image.setAttribute('src', little_image);
+});
+
+close_zoom_figure_image.addEventListener('click', () => {
+    zoom_figure_image.classList.toggle('disable');
 });
